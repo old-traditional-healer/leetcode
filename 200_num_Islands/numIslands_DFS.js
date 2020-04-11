74103a78c0e60e60706e6143a4b7cc8d61bd0f53
DanielchenN <danielchen21@outlook.com>
2020-04-11 15:20:23 +0800

    fix: add 220

--- 200_num_Islands/numIslands_DFS.js
+++ 200_num_Islands/numIslands_DFS.js
@@ -13,7 +13,7 @@ var numIslands = function(grid) {
   let islandNum = 0
   for (let i =0; i<grid.length; i++) {
     for (let j = 0; j< grid[0].length; j++) {
-      if(grid[i][j]) {
+      if(grid[i][j] === '1') {
         infect(grid, i, j)
         islandNum ++ 
       }
@@ -23,7 +23,7 @@ var numIslands = function(grid) {
 }
 
 const infect = function(grid, i, j) {
-  if(i < 0 || j < 0 || i>=grid.length || j>=grid[0].length || grid[i][j] !==1) return 
+  if(i < 0 || j < 0 || i>=grid.length || j>=grid[0].length || grid[i][j] !=='1') return 
   grid[i][j] = '2'
   infect(grid, i + 1, j)
   infect(grid, i - 1, j)

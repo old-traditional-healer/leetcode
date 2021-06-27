a1c95e46dd58c74108d4b7318149577eefe0ad1f
rovast <rovast@163.com>
2021-06-27 09:52:27 +0800

    🎨 二分法定位左右边界
    其中查询了左边界后再查询右边是可以优化的,即数组查询的左起点可以优化

---
+++ 034-find-first-and-last-position-of-element-in-sorted-array/main.js
@@ -0,0 +1,56 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+    const len = nums.length
+    const starIndex = (nums, target) => {
+        let left = 0, right = len - 1
+
+        while (left < right) {
+            const mid = left + Math.floor((right - left) / 2)
+            if (nums[mid] < target) {
+                left = mid + 1
+            } else {
+                right = mid
+            }
+        }
+
+        return nums[left] === target ? left : -1
+    }
+    const endIndex = (nums, target) => {
+        let left = 0, right = len - 1
+
+        // 找到第一个大于 target 的值
+        while (left < right) {
+            const mid = left + Math.floor((right - left) / 2)
+            // console.log('mid=%s,value=%s,target=%s', mid, nums[mid], target)
+            if (nums[mid] > target) {
+                right = mid
+            } else {
+                left = mid + 1
+            }
+        }
+
+        let end = -1
+        if (nums[left] === target) {
+            end = left
+        } else if (nums[left - 1] === target) {
+            end = left - 1
+        }
+
+        return end
+    }
+
+    return [
+        starIndex(nums, target),
+        endIndex(nums, target)
+    ]
+};
+
+
+console.log(searchRange([5, 7, 7, 8, 8, 10], 8))
+console.log(searchRange([5, 7, 7, 8, 8, 10], 6))
+console.log(searchRange([], 0))
+console.log(searchRange([1], 1))

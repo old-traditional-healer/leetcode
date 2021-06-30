408337cdfff9e9c80e88d33ba01415b28112ee1c
rovast <rovast@163.com>
2021-06-30 18:46:12 +0800

    📝 add doc

--- 153-find-minimum-in-rotated-sorted-array/main.js
+++ 153-find-minimum-in-rotated-sorted-array/main.js
@@ -1,4 +1,9 @@
 var findMin = function (nums) {
+    // 1. 进行绘图，可视化分析。原来是一条单调递增的上升线段
+    // 2. 在中间旋转后，其实就是中间切了一刀
+    // 3. 最右侧的是 right, 最左侧的是 left
+    // 4. 只要在右侧段的，需要缩减右侧搜索范围 right=mid
+    // 5. 在左侧的，缩减左侧的范围 left=mid+1
     let left = 0, right = nums.length - 1
     while (left < right) {
         const mid = left + Math.floor((right - left) / 2)

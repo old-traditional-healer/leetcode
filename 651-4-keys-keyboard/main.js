5e04be8d05371370f6ca7d879deddd234d9f6e67
rovast <rovast@163.com>
2021-08-10 10:02:14 +0800

    ✨ 四键键盘
    不同的DP思路对解题的帮助是不一样的.核心是子问题的寻找,找到有意义的子问题

---
+++ 651-4-keys-keyboard/main.js
@@ -0,0 +1,16 @@
+var maxA = function (N) {
+    const dp = new Array(N + 1).fill(0)
+
+    for (let i = 0; i <= N; i++) {
+        // 这次按 A 键
+        dp[i] = dp[i - 1] + 1
+        // 这次按 CtrlV 键，然后比比谁大
+        for (let j = 2; j < i; j++) {
+            // 全选并复制 dp[j-2] 的内容，连续粘贴 i-j 次
+            // 屏幕上 A 的总数是 dp[j-2]*(i-j+1)
+            dp[i] = Math.max(dp[i], dp[j - 2][i - j + 2])
+        }
+    }
+
+    return dp[N]
+}

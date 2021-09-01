3c85e04e338c459e71f92ef2304efc7923fecd0b
rovast <rovast@163.com>
2021-09-01 18:08:08 +0800

    ⬇️ update doc

--- 226-invert-binary-tree/main.js
+++ 226-invert-binary-tree/main.js
@@ -1,6 +1,10 @@
+// 这里使用前序遍历和后序遍历都是可以的
+// 使用中序遍历并不能有效翻转
+// 因为遍历根节点时，会交换左右节点的顺序，如果是中序，则左节点被交换了两次。等于没有翻转
 var invertTree = function (root) {
     if (root == null) return null
 
+    // 前序遍历
     const temp = root.right
     root.right = root.left
     root.left = temp
